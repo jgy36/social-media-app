fc64a6b60a9538e1ceab935793263976e82788ad
jgy36 <jgyoung618@gmail.com>
2025-01-28 11:03:56 -0500

    Initial commit with existing backend and frontend projects

---
+++ PoliticalApp/src/main/java/com/jgy36/PoliticalApp/PoliticalAppApplication.java
@@ -0,0 +1,13 @@
+package com.jgy36.PoliticalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliticalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliticalAppApplication.class, args);
+	}
+
+}
